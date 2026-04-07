5f3692302ab185f4c64f671c81b009e825762185
Túlio Oliveira <tuliooliveirafilipin10@gmail.com>
2026-04-07 18:32:46 -0300

    Header icon svg -> lottie

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react"
 import Header from "./components/Header"
 
+/* eslint-disable */
+
 export default function App() {
+    const browsewrTheme = () => {
+        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+    };
+
+    const [theme, setTheme] = useState(() => {
+        return localStorage.getItem('theme') || browserTheme();
+    });
+
+    useEffect(() => {
+    const sun = document.getElementById('sun');
+    const moon = document.getElementById('moon');
+    if (theme === 'dark') {
+        document.documentElement.classList.add('dark');
+    } else {
+        document.documentElement.classList.remove('dark');
+    }
+
+    localStorage.setItem('theme', theme);
+    }, [theme])
+
     return (
-        <Header />
+        <Header theme={theme} setTheme={setTheme} />
     )
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,20 @@
-export default function Header() {
+import { useEffect } from "react";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
+export default function Header({ theme, setTheme }) {
+
     return (
-        <a href="/" className="logo">
-            <header className="min-w-dvw py-6 bg-white justify-center items-center flex gap-3">
-                <img className="h-14" src="/chef.svg" alt="Chef Logo" />
-                <h1 className="text-4xl text-hero-orange mt-4 tracking-wide">
+        <header className="relative min-w-dvw py-6 bg-white flex justify-center items-center">
+            <a href="/" className="logo justify-center items-center flex gap-1">
+                <DotLottieReact src="/chef.lottie" layout={{ fit: 'cover', alignment: [0.5, 0.5] }} autoplay style={{ width: 60, height: 84 }} />
+                <h1 className="text-4xl text-hero-orange mt-4 sm:tracking-wide">
                     <span className="font-[Sora]">Chef</span>
                     <span className="font-[Sora] font-medium">Byte</span>
                 </h1>
-            </header>
-        </a>
+            </a>
+            <button id="change-theme" className="absolute" onClick={setTheme}>
+                
+            </button>
+        </header>
     );
 };
